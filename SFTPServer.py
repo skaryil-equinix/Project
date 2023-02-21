ceeb8bd402212e76b2e1d9b47f0406c0c471d8b7
adityarathi7 <adrathi@equinix.com>
2023-02-21 22:56:46 +0530

    added libraries and regex support

--- SFTPServer.py
+++ SFTPServer.py
@@ -1,8 +1,9 @@
 from FileLocation import FileLocation
 import pysftp
+import re
 
-class SFTPServer(FileLocation) :
 
+class SFTPServer(FileLocation):
     cnOpts = pysftp.CnOpts()
     cnOpts.hostkeys = None
 
@@ -11,7 +12,20 @@ class SFTPServer(FileLocation) :
     password = None
     remotePath = None
     localPath = None
+    regex_pattern = None
+    filename_matching_regex = []
 
-    def sftp_list_files(self):
-        with pysftp.Connection(self.hostName, username= self.userName, password=self.password,cnopts=self.cnOpts) as sftp:
-            sftp.get(self.remotePath,self.localPath)  # get a remote file
+    def set_connection_to_database(self):
+        with pysftp.Connection(self.hostName, username=self.userName, password=self.password,
+                               cnopts=self.cnOpts) as sftp:
+            print("Connected to server")
+            sftp.cwd("/")
+            sftp.get(self.remotePath, self.localPath)  # get a remote file
+            list_dir = sftp.listdir_attr()
+
+            for filename in list_dir:
+                if re.search(self.regex_pattern, filename):
+                    self.filename_matching_regex.append(filename)
+
+    def read_directory_with_regrex(self):
+        return self.filename_matching_regex
